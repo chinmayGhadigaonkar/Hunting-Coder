c1cd2a8a96ca7a5c3142bf4ad58d55abfa5d2894
chinmayGhadigaonkar <ghadigaonkarchinmay@gmail.com>
2023-06-09 19:12:43 +0530

    First commit

---
+++ app/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="h-16  flex  justify-center items-center">
+        <ul className='flex  justify-center items-center  mx-3 '>
+          <Link href="/">
+          <li className='mx-4 text-lg font-bold hover:cursor-pointer hover:underline'>Home</li>
+          </Link>
+            <Link href="/about">
+            <li className='mx-4 text-lg font-bold hover:cursor-pointer hover:underline'>About </li>
+            </Link >
+            <Link href="/blog">
+            <li className='mx-4 text-lg font-bold hover:cursor-pointer hover:underline'>Blog </li>
+            </Link>
+            <Link href="/contactus">
+            <li className='mx-4 text-lg font-bold hover:cursor-pointer hover:underline'>Contact Us</li>
+            </Link>
+            
+            
+            
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ app/blog/page.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>
+      <div className="mt-6 mb-3 m-auto   w-[20rem] ">
+        <h2 className=" text-2xl font-bold my-2  ">Popular Blogs</h2>
+        <div className="card mt-4 ">
+          <h3 className="font-semibold  text-lg "> JavaScipt Blog</h3>
+          <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.</p>
+        </div>
+        <div className="card mt-2">
+          <h3 className="font-semibold text-lg  "> JavaScipt Blog</h3>
+          <p>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit.</p>
+        </div>
+        <div className="card mt-2">
+          <h3 className="font-semibold  text-lg "> JavaScipt Blog</h3>
+          <p>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit.</p>
+        </div>
+        <div className="card mt-2">
+          <h3 className="font-semibold text-lg  "> JavaScipt Blog</h3>
+          <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.
+            .</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default blog
